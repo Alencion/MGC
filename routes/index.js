b72cbcfedf6ac131ce31980b8700bca2a3ae083e
Alencion <rhqnrdl625@naver.com>
2019-01-12 14:58:47 +0900

    make board page html and css

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var {User} = require('../models');
-/* GET home page. */
 
+/* GET home page. */
 router.get('/', function(req, res, next) {
   if(req.isAuthenticated()) {
     User = req.user;
-    console.log(User);
   }
   res.render('index', {
     title: 'Steam' ,
