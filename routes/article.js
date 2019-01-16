38279ee4f94f8a789f9caa64deb2f98ff5601688
rinha7 <eocjstnals12@gmail.com>
2019-01-16 13:14:11 +0900

    make forgot password

--- routes/article.js
+++ routes/article.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var {User, Article} = require('../models');
+var path = require('path');
+var multer = require('multer');
+var {isLoggedIn, isNotLoggedIn} = require('./middlewares');
+
 
 /* GET article, write page. */
 router.get('/id=:id' , function (req, res, next) {
@@ -24,8 +28,20 @@ router.get('/write', function (req, res, next) {
     });
 });
 /* POST write page. */
-router.post('/write', function (req, res, next) {
-    res.send('POST request to the homepage');
+var upload = multer();
+router.post('/write', isLoggedIn, upload.none(), async(req, res, next)=>{
+    try{
+        var post = await Article.create({
+            title: req.body.title,
+            description : req.body.description,
+            view: 0,
+            user_id : req.user.id,
+        });
+        res.redirect('/board');
+    }catch(error){
+        console.error(error);
+        next(error);
+    }
 });
 
 module.exports = router;
