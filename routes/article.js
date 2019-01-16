3c0f774f5736bba55e107815b4d0eb92f984b541
Alencion <rhqnrdl625@naver.com>
2019-01-16 21:13:22 +0900

    steam web application ver 0.1

--- routes/article.js
+++ routes/article.js
@@ -7,12 +7,14 @@ var {isLoggedIn, isNotLoggedIn} = require('./middlewares');
 
 /* GET article, write page. */
 router.get('/id=:id' , async(req, res, next) => {
-
     try{
+        //select article
         var id = req.params.id;
         var article = await Article.find({where: { id }});
-        var id = article.user_id;
-        var writer = await User.find({where: { id }})
+        //select user to take article writer
+        var writer = await User.find({where: { id: article.user_id }});
+        //update view + 1
+        // Article.update({view: article.view+1 }, {where: {id}});
     }
     catch (error) {
         console.error(error);
@@ -26,8 +28,8 @@ router.get('/id=:id' , async(req, res, next) => {
         isNotSignedIn: !req.isAuthenticated(),
         username : User.name,
         article_title : article.title,
-        views : article.view,
-        date : article.created_date,
+        view : article.view,
+        created_date : article.created_date,
         writer : writer.name,
         description : article.description,
     });
@@ -46,13 +48,12 @@ router.get('/write', function (req, res, next) {
 var upload = multer();
 router.post('/write', isLoggedIn, upload.none(), async(req, res, next)=>{
     try{
-        var post = await Article.create({
+        await Article.create({
             title: req.body.title,
             description : req.body.description,
             view: 0,
             user_id : req.user.id,
         });
-        console.log(req.body.description);
         res.redirect('/board');
     }catch(error){
         console.error(error);
--- routes/authorization.js
+++ routes/authorization.js
@@ -3,6 +3,7 @@ var passport = require('passport');
 var bcrypt = require('bcrypt');
 var {isLoggedIn, isNotLoggedIn} = require('./middlewares');
 var {User} = require('../models');
+var {tmpUser} = require('../models');
 
 var router = express.Router();
 
@@ -14,7 +15,22 @@ router.get('/signin', function (req, res, next) {
 });
 router.get('/signup', function (req, res, next) {
     res.render('signup');
-
+});
+router.get('/signout', isLoggedIn, (req, res)=>{
+    req.logout();
+    req.session.destroy();
+    res.redirect('/');
+});
+router.get('/lostpw', function (req, res, next) {
+    res.render('lostpw',{
+        user:req.user
+    });
+});
+router.get('/reset', function(req, res, next){
+    console.log(tmpUser.client_id);
+    res.render('reset',{
+        user:req.user
+    });
 });
 
 /* POST SignIn, SignUp page. */
@@ -39,8 +55,6 @@ router.post('/signin', isNotLoggedIn, (req, res, next)=>{
     })(req, res, next);
     //res.redirect('/');
 });
-
-
 router.post('/signup',isNotLoggedIn, async(req, res, next)=>{
     var {client_id, client_pw, name, e_mail} = req.body;
     try {
@@ -65,5 +79,39 @@ router.post('/signup',isNotLoggedIn, async(req, res, next)=>{
     res.send('POST request to the homepage');
 });
 
+router.post('/lostpw',isNotLoggedIn,  async (req, res, next)=> {
+    try {
+        exUser = await User.find({where: {e_mail: req.body.e_mail}});
+        //console.log(exUser);
+        if (!exUser) {
+            req.flash('loginError', '존재하지 않는 계정입니다.');
+            return res.redirect('/auth/signin');
+        }
+        tmpUser = exUser;
+        return res.redirect('/auth/reset');
+    }catch(error){
+        console.error(error);
+        return next(error);
+    }(req,res,next);
+});
+router.post('/reset', isNotLoggedIn, async(req,res,next)=>{
+    try {
+        if (!tmpUser) {
+            req.flash('loginError', '삐용삐용 에러입니다');
+            return res.redirect('/auth/signin');
+        }
+        var hash = await bcrypt.hash(req.body.password, 12);
+        User.update({
+            client_pw:hash,
+        },{
+            where:{id:tmpUser.id},
+        });
+        req.flash('loginError', '비밀번호가 성공적으로 변경되었습니다!');
+        return res.redirect('/auth/signin');
+    }catch(error){
+        console.error(error);
+        return next(error);
+    }
+})
 
 module.exports = router;
